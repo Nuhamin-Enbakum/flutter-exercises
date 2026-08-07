597b278822b9a87f26ae07f0b4b368e413b77721
Nuhamin Enbakum <nuham1941@gmail.com>
2026-08-07 11:22:33 +0300

    Add login screen with form validation password toggle, and social login buttons

--- lib/main.dart
+++ lib/main.dart
@@ -12,185 +12,297 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: ProfileCard()
-          ),
+          child: LoginScreen()
+        ),
       ),
     );
   }
 }
 
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscurePassword = true;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(24),
       constraints: const BoxConstraints(maxWidth: 400),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
+        color: Color(0xFF121212),
+        borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.black26,
+            color: Colors.grey,
             spreadRadius: 5,
             blurRadius: 7,
             offset: Offset(0, 3)
+
           ),
         ],
       ),
+      
+      child: Form(
+        key: _formKey,
       child: Column(
+        spacing: 16,
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Stack(
-            clipBehavior: Clip.none,
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(Icons.star, color: Colors.black, size: 28),
+          ),
+          
+          Text(
+            'Login to Storytale',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18
+              
+            ),
+          ),
+          Text(
+            'Grab high-class graphics and book your design workflow',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 10),
+
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              hintText: 'Email or useranme',
+              filled: true,
+              fillColor: Colors.black,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey),
+              )
+            ),
+            validator: (value) {
+              if (value == null || value.trim().isEmpty) {
+                return 'Please enter your email or username';
+              }
+              return null;
+            },
+          ),
+          
+          TextFormField(
+            controller: _passwordController,
+            obscureText: _obscurePassword,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              filled: true,
+              fillColor: Colors.black,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  setState(() {
+                    _obscurePassword = !_obscurePassword;
+                  });
+                },
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your password';
+              }
+              if (value.length < 6){
+                return 'Password must be at least 6 characters';
+              }
+              return null;
+            },
+          ),
+
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                if(_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Logging in...')),
+                  );
+                }
+
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 171, 226, 7),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                
+              ),
+              child: Text(
+                'Log in',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+            ),
+          ),
+
+          Row(
             children: [
-              Container(
-                height: 150,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Colors.lightBlue, Colors.blue, Colors.indigo]
-                    ),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10)
+              Expanded(
+                child: Divider(
+                  color: Colors.grey,
+                  thickness: 0.3,
+                ),
+                
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  'or authorize with',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+
                   ),
                 ),
+              ),
+              Expanded(
+                child: Divider(
+                  color: Colors.grey,
+                  thickness: 0.3,
                 ),
-                const Positioned(
-                  bottom: -40,
-                  left: 20,
-                  child: CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage('assets/images/First.jpg'),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.grey),
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    
-                        
-                       ),
-                       const Positioned(
-                      top: 10,
-                      right: 10,
-                      child: CircleAvatar(
-                        radius: 15,
-                        backgroundColor: Colors.white,
-                        child: Icon(
-                          Icons.edit
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.g_mobiledata, color: Colors.grey),
+                      SizedBox(width: 5),
+                      Text(
+                        'Google',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15
 
                         ),
                       ),
-                ),
-            ]
-          ),
+                    ],
+                  ),
+                )
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.grey),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
 
-          const SizedBox(height: 50),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              spacing: 20,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'Benjamin Lewis',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.apple, color: Colors.grey),
+                      SizedBox(width: 5),
+                      Text(
+                        'Apple',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+
+                    ],
                   ),
                 ),
-                Text(
-                  'Product Designer | Senior User Experience Designer',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Row(
-            children: [
-              Icon(Icons.location_on, size: 16),
-              SizedBox(width: 5),
-              Text(
-                'Los Ageles, California, United States of America',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                ),
+              ),
+            ],
 
-              ),
-            ],
           ),
-          Text(
-            'As a product designer with UX experience, I am passionate about creating products that meet the needs of users while delivering a beautiful and intuitive experience. I understand that design goes beyond asthetics and must be rooted in user needs and behavior.',
-            style: TextStyle(fontSize: 12),
-          ),
-          Row(
-            children: [
-              Icon(Icons.email, size: 16, color: Colors.grey),
-              SizedBox(width: 5),
-              Text(
-                'lewis@example.com',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Icon(Icons.calendar_today, size: 16, color: Colors.grey),
-              SizedBox(width: 5),
-              Text(
-                '12 Dec 1987',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Icon(Icons.phone, size: 16, color: Colors.grey),
-              SizedBox(width: 5),
-              Text(
-                '(222) 555-1234',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Joined on 12 January 2025',
-                style: TextStyle(fontSize: 10),              
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.link, size: 20),
-                    SizedBox(width: 10),
-                    Icon(Icons.public, size: 20),
-                    SizedBox(width: 10),
-                    Icon(Icons.share, size: 20),
-                  ],
-                ),
-            ],
-          ),
-              ],
+
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              padding: EdgeInsets.zero,
             ),
-            
-              
-          ),
-          
-          
+            child: Text(
+              'Forgot password?',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
 
+          ),
+
+          Row(
+            
+            children: [
+              Text(
+                "Don't have an account?",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(width: 5),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                ),
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          
         ],
+        
+      ),
       ),
     );
-
   }
 }
