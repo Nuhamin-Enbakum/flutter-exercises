ad4a305a622e90c19d3989228630816170e995f5
Nuhamin Enbakum <nuham1941@gmail.com>
2026-08-10 19:40:09 +0300

    Add dispose() method for the controllers

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,13 @@ class _LoginScreenState extends State<LoginScreen> {
   final _passwordController = TextEditingController();
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(24),
